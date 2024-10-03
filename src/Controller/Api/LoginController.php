2ff585fa24e88eb08b634fd562dd254be6cb274b
MohamedMOSLAH <mohamed.amine.moslah@gmail.com>
2024-10-04 00:55:06 +0200

    update login

---
+++ src/Controller/Api/LoginController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller\Api;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class LoginController extends AbstractController
+{
+
+    #[Route(path: "/api/login", name: "api_login", methods: ["POST"])]
+    public function ApiLogin() {
+        /** @var User */
+        $user = $this->getUser();
+
+        $userData = [
+            'email' => $user->getEmail(),
+            'firstName' => $user->getFirstName(),
+            'last_name' => $user->getLastName(),
+
+        ];
+
+        return $this->json($userData);
+    }
+}

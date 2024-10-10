3b368aa03b903582aaaefa7029593e632b723c9f
MohamedMOSLAH <mohamed.amine.moslah@gmail.com>
2024-10-10 20:21:36 +0200

    update register

--- src/Controller/Api/RegisterController.php
+++ src/Controller/Api/RegisterController.php
@@ -5,19 +5,27 @@ namespace App\Controller\Api;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
 use App\Security\SecurityAuthenticator;
+use JMS\Serializer\SerializerInterface;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 
 class RegisterController extends AbstractController {
 
-    #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $entityManager): Response
+    #[Route('/api/register', name: 'app_register')]
+    public function register(SerializerInterface $serializer, Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $entityManager): Response
     {
+
+        if($this->getUser()){
+            return new JsonResponse($serializer->serialize(['message' => "you must logout into get register page"], 'json'), Response::HTTP_UNAUTHORIZED, [], true);
+        }
+
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);

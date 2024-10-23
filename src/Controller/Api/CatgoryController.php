104071b78e7b7016f3e74ed13ee6d8c49d63e74b
MohamedMOSLAH <mohamed.amine.moslah@gmail.com>
2024-10-23 22:56:54 +0200

    update Category

--- src/Controller/Api/CatgoryController.php
+++ src/Controller/Api/CatgoryController.php
@@ -2,15 +2,18 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\Category;
+use JMS\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CategoryController extends AbstractController 
 {
     #[Route('api/category/new', name:"api_category_add", methods:['POST'])]
-    public function addCategory():JsonResponse
+    public function addCategory(SerializerInterface $serializer, Request $request):JsonResponse
     {
-        
+        $category = $serializer->deserialize( $request->getContent(), Category::class, "json" );
     }
 }

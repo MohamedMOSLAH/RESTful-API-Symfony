14b86362b93ed45d230200d37f1149c59157c629
MohamedMOSLAH <mohamed.amine.moslah@gmail.com>
2024-10-24 23:07:15 +0200

    function addCategory

--- src/Controller/Api/CatgoryController.php
+++ src/Controller/Api/CatgoryController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Api;
 
 use App\Entity\Category;
+use Doctrine\ORM\EntityManagerInterface;
 use JMS\Serializer\SerializerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,8 +13,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class CategoryController extends AbstractController 
 {
     #[Route('api/category/new', name:"api_category_add", methods:['POST'])]
-    public function addCategory(SerializerInterface $serializer, Request $request):JsonResponse
+    public function addCategory(SerializerInterface $serializer, Request $request, EntityManagerInterface $entityManager):JsonResponse
     {
         $category = $serializer->deserialize( $request->getContent(), Category::class, "json" );
+
+        $entityManager->persist($category);
+        $entityManager->flush();
+
+        return new JsonResponse('ok');
     }
 }
